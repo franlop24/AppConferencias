f45e25905737e41f82d263a206e159eac2d8cbdb
Francisco Lopez <franlopbri@gmail.com>
2020-10-25 11:51:08 -0600

    se crean fragmente y activitities

---
+++ app/src/main/java/com/franlops/conf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.franlops.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.franlops.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
