951e6e92a8b14fd83b0e920f8210ebfc09d13be5
Francisco Lopez <franlopbri@gmail.com>
2020-11-22 19:46:13 -0600

    importando firebase y creacion de Clases de Kotlin

---
+++ app/src/main/java/com/franlops/conf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.franlops.conf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
